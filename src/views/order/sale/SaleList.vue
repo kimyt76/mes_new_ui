fc38edf49194ffcbe6ae475cd0edc0989f32e084
kimyt <kimyt@jincostech.com>
2025-08-20 16:44:14 +0900

    디자인 변경

--- src/views/order/sale/SaleList.vue
+++ src/views/order/sale/SaleList.vue
@@ -1,125 +1,146 @@
 <template>
 <v-breadcrumbs
-  :items="['MES', '수주관리', '판매관리']"
-  class="custom-breadcrumbs"
-  />
-    <v-card class="pa-1" style="height: 60px;">
-    <v-row>
-      <v-form ref="srhForm" @submit.prevent ='srhSaleList'>
-      <v-col class="d-flex flex-row ga-3 ml-2 mr-2">
+    :items="['MES', '수주관리', '판매관리']"
+    class="custom-breadcrumbs"
+    />
+  <v-card class="pa-1" style="height: 60px;">
+    <v-card-text >
+      <v-row>
+      <v-form ref="srhForm" @submit.prevent="srhSaleList">
+        <v-col class="d-flex flex-row ga-3">
           <v-text-field
-            v-model="form.itemName"
-            label="품목명"
-            placeholder="품목명을 입력해주세요"
+              v-model="form.itemName"
+              dense
+              density="compact"
+              label="품목명"
+              placeholder="품목명을 입력해주세요"
+              variant="underlined"
+              style="width: 180px;"
+              />
+          <v-text-field
+              v-model="form.managerName"
+              dense
+              density="compact"
+              label="담당자명"
+              placeholder="담당자명을 입력해주세요"
+              variant="underlined"
+              style="width: 180px;"
+              />
+          <v-text-field
+              v-model="form.customerName"
+              dense
+              density="compact"
+              label="거래처명"
+              placeholder="거래처명을 입력해주세요"
+              variant="underlined"
+              style="width: 150px;"
+              />
+          <v-select
+            v-model="form.statusType"
+            label="진행상태"
+            :items="statusTypes"
+            item-title="codeNm"
+            item-value="code"
             variant="underlined"
             density="compact"
-            style="width: 150px;"
-            />
-          <v-text-field
-            v-model="form.managerName"
-            label="담당자명"
-            placeholder="담당자을 입력해주세요"
-            variant="underlined"
-            density="compact"
-            style="width: 150px;"
-            />
-          <v-text-field
-            v-model="form.customerName"
-            label="거래처명"
-            placeholder="거래처명을 입력해주세요"
-            variant="underlined"
-            density="compact"
-            style="width: 150px;"
+            style="width: 100px;"
             />
           <v-btn
             text="조회"
-            variant="tonal"
+            color="brown-lighten-4"
             type="submit"
             />
           <v-btn
             text="초기화"
             @click="srhForm.reset()"
             />
-          </v-col>
-        </v-form>
+        </v-col>
+      </v-form>
     </v-row>
+    </v-card-text>
   </v-card>
-  <v-row style="height: 70px;">
-      <v-col class="d-flex justify-end align-center mr-2" style="gap: 8px; margin-top: 8px;">
-        <v-btn
-            dense
-            color="brown-lighten-4"
-            text="신규"
-            @click="gosale()"
-            />
-          <v-btn
-            class="mr-2 excel-btn"
-            text="엑셀"
-            prepend-icon="mdi-microsoft-excel"
-            @click="excel"
-            />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-data-table
-          :headers="headers"
-          :items="saleList"
-          :loading="loading"
-          no-data-text="데이터가 없습니다."
-          loading-text="조회중입니다 잠시만 기다려주세요"
-          :items-per-page="15"
-          >
-      <template v-slot:headers="{ columns }">
-        <tr>
-          <th
-            v-for="column in columns"
-            :key="column.key"
-            class="custom-header"
-            style="height: 30px;"
-            :style="{textAlign: 'center' } "
-            >
-            {{ column.title }}
-          </th>
-        </tr>
-      </template>
-      <!-- 리스트 영역 커스터마이징 -->
-      <template #item="{ item }">
-        <tr style="height: 30px;">
-          <td style="width: 100px; height: 30px; text-align: center; text-decoration: underline; cursor: pointer;" @click="gosale(item.saleId)">{{ item.saleDateSeq }}</td>
-          <td style="width: 200px; height: 30px; text-decoration: underline;  cursor: pointer;" @click="gosale(item.saleId)">{{item.itemName}}</td>
-          <td style="width: 90px; height: 30px; text-align: center;">{{item.managerName}}</td>
-          <td style="width: 200px; height: 30px; text-align: left;">{{item.customerName}}</td>
-          <td style="width: 100px; height: 30px; text-align: right;">{{ formatComma(item.totPrice) }}</td>
-          <td style="width: 90px; height: 30px; text-align: center;">{{item.transactionTypeName}}</td>
-          <td style="width: 90px; height: 30px; text-align: center;">{{item.descStorageName}}</td>
-          <td style="width: 40px; height: 30px; text-align: center;">
-            <p
-            style="padding: 4px; text-align: center; cursor: pointer;"
-            :style="{
-              backgroundColor: item.printYn === 'Y' ? '#FFAB91' : 'transparent'
-            }"
-              @click="onPrint"
+  <v-spacer></v-spacer>
+  <v-row>
+    <v-col class="d-flex justify-end align-center mr-2" style="gap: 10px; margin-top: 10px;">
+      <v-btn
+        color="brown-lighten-4"
+        text="신규"
+        @click="selectRowClick"
+        />
+      <v-btn
+        class="excel-btn"
+        text="엑셀"
+        prepend-icon="mdi-microsoft-excel"
+        @click="excel"
+        />
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col class="pa-0">
+      <v-data-table
+        :headers="headers"
+        :items="contractList"
+        :loading="loading"
+        :items-per-page="15"
+        no-data-text="데이터가 없습니다."
+        loading-text="조회중입니다 잠시만 기다려주세요"
+        density="compact"
+        fixed-header
+        height="720px"
+        >
+        <template v-slot:headers="{ columns }">
+          <tr>
+            <th
+              v-for="column in columns"
+              :key="column.key"
+              class="custom-header"
+              style="height: 40px;"
+              :style="{textAlign: 'center'} "
               >
-              인쇄
-            </p>
-          </td>
-          <td style="width: 60px; height: 30px; text-align: center; cursor: pointer;" @click="goSaleItem(item.saleId, item.saleDateSeq)">주문서</td>
-        </tr>
-      </template>
-    </v-data-table>
-      </v-col>
-    </v-row>
+              {{ column.title }}
+            </th>
+          </tr>
+        </template>
 
-    <v-dialog v-model="dialog" width="1000px" height="400px" persistent>
+        <template #item.saleDateSeq="{ item, index }">
+          <div
+            style="cursor: pointer; text-decoration: underline; "
+            @click="selectRowClick(item, index)"
+          >
+            {{ item.saleDateSeq }}
+          </div>
+        </template>
+        <template #item.itemName="{ item, index }">
+          <div
+            style="cursor: pointer; text-decoration: underline;"
+            @click="selectRowClick(item, index)"
+          >
+            {{ item.itemName }}
+          </div>
+        </template>
+        <template #item.statusType="{ item, index }">
+            {{ item.statusType === 'ING' ?  '진행중' : '종료'}}
+        </template>
+        <template #item.printYn="{ item, index }">
+           <p style="padding: 4px;
+              text-align: center;
+              cursor: pointer;"
+              :style="{backgroundColor: item.printYn === 'Y' ? '#FFAB91' : 'transparent'}"
+              @click="onPrint"
+            >인쇄
+          </p>
+        </template>
+      </v-data-table>
+    </v-col>
+  </v-row>
+
+  <v-dialog v-model="dialog" width="1000px" height="400px" persistent>
       <SalePop
         :id="saleId"
         :no="saleDateSeq"
         @close-dialog="dialog = false"
         />
     </v-dialog>
-
-
 </template>
 
 <script setup>
@@ -143,28 +164,28 @@ const form = reactive({
   customerCd: '',
 })
 
-const headers = ref([
-  { title: '일자-No.',          key: 'saleDateSeq',   align: 'center' },
-  { title: '품목명',            key: 'itemName',      align: 'start'},
-  { title: '담당자명',          key: 'managerName',   align: 'center' },
-  { title: '거래처명',          key: 'customerName',  align: 'start' },
-  { title: '금액 합계',         key: 'totPrice',      align: 'end'},
-  { title: '거래유형명',        key: 'totUnitPrice',  align: 'center'},
-  { title: '창고명',            key: 'statusType',    align: 'center' },
-  { title: '인쇄',              key: 'printYn',       align: 'center' },
-  { title: '주문서',            key: 'saleId',        align: 'center' },
-])
 
+const headers = ref([
+  { title: '일자-No.',          key: 'saleDateSeq',   align: 'center' , width: '90px'},
+  { title: '품목명',            key: 'itemName',      align: 'start', width: '280px'},
+  { title: '담당자명',          key: 'managerName',   align: 'center' , width: '90px'},
+  { title: '거래처명',          key: 'customerName',  align: 'start' , width: '200px'},
+  { title: '금액 합계',         key: 'totPrice',      align: 'end', width: '100px'},
+  { title: '거래유형명',        key: 'totUnitPrice',  align: 'center', width: '100px'},
+  { title: '창고명',            key: 'statusType',    align: 'center', width: '150px' },
+  { title: '인쇄',              key: 'printYn',       align: 'center', width: '40px' },
+  { title: '주문서',            key: 'saleId',        align: 'center', width: '80px' },
+])
 
 onMounted( async () => {
   srhSaleList()
 })
 
-const gosale = (id) =>{
-  if ( isEmpty(id)) {
+const selectRowClick = (item, index) =>{
+  if ( isEmpty(item.saleId)) {
     router.push({ name: 'SaleReg' })
   }else{
-    router.push({ name: 'SaleDetail', params: { id } })
+    router.push({ name: 'SaleDetail', params: { id: item.saleId } })
   }
 }
 
@@ -189,10 +210,11 @@ const goSaleItem = (id, no) =>{
 
 
 const excel = () => {
-  exportToExcel(headers, saleList.value, '수주_목록')
+  exportToExcel(headers, saleList.value, '판매_목록')
 }
+
 </script>
 
-<style scoped>
+<style>
 @import '@/assets/css/main.css';
 </style>
