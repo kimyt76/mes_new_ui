d9098b83f115fac002e37fa4f2d4e84da70a933c
kimyt <kimyt@jincostech.com>
2025-08-22 08:36:53 +0900

    디자인 변경

--- src/views/lab/ingredient/IngredientList.vue
+++ src/views/lab/ingredient/IngredientList.vue
@@ -1,116 +1,107 @@
 <template>
-  <v-breadcrumbs
+<v-breadcrumbs
     :items="['MES', '연구관리', '성분정보']"
     class="custom-breadcrumbs"
     />
-  <v-card
-    class="pa-3"
-    color="blue-grey-lighten-5"
-     style="height: 60px;"
-    >
-    <v-form ref="srhForm" @submit.prevent="srhList">
+  <v-card class="pa-1" style="height: 60px;">
+    <v-card-text >
       <v-row>
-        <v-col style="background-color: #ECEFF1">
-          <div class="d-flex ga-4"> <!-- ga-4 는 Vuetify gap 클래스 -->
+      <v-form ref="srhForm" @submit.prevent="srhList">
+        <v-col class="d-flex flex-row ga-3">
           <v-text-field
-            v-model="form.ingredientName"
-            label="성분명"
-            placeholder="성분명을 입력해주세요"
-            variant="underlined"
-            density="compact"
-            />
+              v-model="form.ingredientName"
+              density="compact"
+              label="성분명"
+              placeholder="성분명을 입력해주세요"
+              variant="underlined"
+              style="width: 180px;"
+              />
           <v-text-field
-            v-model="form.functionNm"
-            label="function명"
-            placeholder="function명을 입력해주세요"
-            variant="underlined"
-            density="compact"
-            />
+              v-model="form.functionNm"
+              density="compact"
+              label="function명"
+              placeholder="function명을 입력해주세요"
+              variant="underlined"
+              style="width: 180px;"
+              />
           <v-text-field
-            label="등록자"
-            placeholder="등록자를 입력해주세요"
-            variant="underlined"
-            density="compact"
-            />
+              v-model="form.regName"
+              density="compact"
+              label="등록자"
+              placeholder="등록자명을 입력해주세요"
+              variant="underlined"
+              style="width: 150px;"
+              />
           <v-btn
-            color="#EFEBE9"
-            class="mt-2"
-            type="submit"
-            density="compact"
             text="조회"
+            color="brown-lighten-4"
+            type="submit"
             />
           <v-btn
-            @click="reset"
-            class="mt-2"
-            density="compact"
             text="초기화"
+            @click="srhForm.reset()"
             />
-        </div>
-      </v-col>
-      </v-row>
-    </v-form>
+        </v-col>
+      </v-form>
+    </v-row>
+    </v-card-text>
   </v-card>
-  <v-row class="mb-1">
-  <v-col>
-    <div class="d-flex ga-4 justify-end">
-    <v-btn
-        color = "brown-lighten-4"
-        class="mt-3"
-        density="compact"
-        @click="openPop('N')"
+  <v-spacer></v-spacer>
+  <v-row>
+    <v-col class="d-flex justify-end align-center mr-2" style="gap: 10px; margin-top: 10px;">
+      <v-btn
+        color="brown-lighten-4"
         text="신규"
+        @click="selectRowClick"
         />
       <v-btn
-        class="mt-3 mr-3 excel-btn"
+        class="excel-btn"
         text="엑셀"
-        density="compact"
         prepend-icon="mdi-microsoft-excel"
         @click="excel"
         />
-     </div>
-  </v-col>
-</v-row>
-<div class="table-wrapper">
-  <v-data-table
-    :headers="headers"
-    :loading="loading"
-    :items="ingredientList"
-    class="scrollable-table"
-    style="height: 700px;"
-    :items-per-page="25"
-    no-data-text="데이터가 없습니다."
-    loading-text="조회중입니다 잠시만 기다려주세요"
-    >
-    <template v-slot:headers="{ columns }">
-      <tr style="height: 40px;">
-        <th
-          v-for="column in columns"
-          :key="column.key"
-          class="custom-header"
-          style="height: 40px;"
-          >
-          {{ column.title }}
-        </th>
-      </tr>
-    </template>
-    <!-- 리스트 영역 커스터마이징 -->
-    <template #item="{ item }">
-      <tr style="height: 30px;">
-        <td class="custom-row" style="width: 100px; height: 30px; padding: 0; cursor: pointer;" @click="handleRowClick(item.ingredientCode)">{{ item.ingredientCode }}</td>
-        <td style="width: 200px; height: 30px;">{{item.krIngredientName}}</td>
-        <td style="width: 200px; height: 30px;">{{item.enIngredientName}}</td>
-        <td style="width: 200px; height: 30px;">{{item.cnIngredientName}}</td>
-        <td style="width: 100px; height: 30px;">{{item.casNo}}</td>
-        <td style="width: 150px; height: 30px;">{{item.functionNm}}</td>
-        <td class="custom-row"  style="width: 100px; height: 30px;">{{item.updNm}}</td>
-        <td class="custom-row"  style="width: 100px; height: 30px;">{{item.updDate}}</td>
-        <td style="width: 100px; height: 30px;">{{item.etc}}</td>
-      </tr>
-    </template>
-  </v-data-table>
-</div>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col class="pa-0">
+      <v-data-table
+        :headers="headers"
+        :items="ingredientList"
+        :loading="loading"
+        :items-per-page="15"
+        no-data-text="데이터가 없습니다."
+        loading-text="조회중입니다 잠시만 기다려주세요"
+        density="compact"
+        fixed-header
+        height="690px"
+        >
+        <template v-slot:headers="{ columns }">
+          <tr>
+            <th
+              v-for="column in columns"
+              :key="column.key"
+              class="custom-header"
+              style="height: 40px;"
+              :style="{textAlign: 'center'} "
+              >
+              {{ column.title }}
+            </th>
+          </tr>
+        </template>
 
-<v-dialog  v-model="dialog" max-width="800px" persistent>
+        <template #item.ingredientCode="{ item, index }">
+          <div
+            style="cursor: pointer; text-decoration: underline; width: 95%;"
+            @click="selectRowClick(item, index)"
+          >
+            {{ item.ingredientCode }}
+          </div>
+        </template>
+      </v-data-table>
+    </v-col>
+  </v-row>
+
+  <v-dialog  v-model="dialog" width="800px" height="750px" persistent>
     <IngredientDetailPop
       :title="title"
       :mode="mode"
@@ -118,7 +109,6 @@
       @saved="handleSaved"
       @close-dialog="dialog = false"/>
   </v-dialog>
-
 </template>
 
 <script setup>
@@ -127,6 +117,7 @@ import {onMounted, reactive, ref} from 'vue'
 import { exportToExcel } from '@/util/exportToExcel';
 import { useAlertStore } from '@/stores/alert';
 import IngredientDetailPop from './IngredientRegPop.vue';
+import { isEmpty, formatComma, todayKST, formatDate } from '@/util/common';
 
 const { vError, vSuccess } = useAlertStore()
 const ingredientCode = ref('')
@@ -134,12 +125,50 @@ const dialog = ref(false)
 const loading = ref(false)
 const srhForm = ref(null)
 const ingredientList = ref([])
+const mode = ref('')
+const title = ref('')
+
 const form = reactive({
   ingredientName: '',
   functionNm : '',
   casNo: '',
 })
 
+const headers = ref([
+    { title: '성분코드',    key: 'ingredientCode',   align: 'center',  width: '70px'},
+    { title: '국문성분명',  key: 'krIngredientName',   align: 'start',width: '240px'},
+    { title: '영문성분명',  key: 'enIngredientName',   align: 'start', width: '240px'},
+    { title: '중문성분명',  key: 'cnIngredientName',   align: 'start', width: '250px'},
+    { title: 'CAS No',     key: 'casNo',              align: 'start', width: '150px'},
+    { title: 'function',   key: 'functionNm',     align: 'start', width: '150px'},
+    { title: '등록자',      key: 'regName',     align: 'center', width: '70px'},
+    { title: '수정자',      key: 'updName',     align: 'center', width: '70px'},
+    { title: '수정일자',    key: 'updDate',     align: 'center', width: '100px'},
+    { title: '비고',        key: 'etc',         align: 'start', width: '110px'},
+])
+
+
+const selectRowClick = (item, index) => {
+  console.log('item',item.ingredientCode)
+  if ( isEmpty(item.ingredientCode)) {
+    mode.value = 'N'
+    title.value = '성분 등록'
+  }else{
+     mode.value= 'U'
+     title.value = '성분 상세정보'
+     ingredientCode.value = item.ingredientCode
+  }
+  dialog.value = true
+}
+
+const handleSaved = (msg) => {
+  vSuccess(msg)
+  srhList()
+}
+
+/**
+ * 리스트 조회
+ */
 const srhList = async () => {
   loading.value = true
 
@@ -156,34 +185,6 @@ const srhList = async () => {
   }
 }
 
-/**
- *  테이블 헤더
- */
-const headers = ref([
-    { title: '성분코드',    key: 'ingredientCode', width: 100},
-    { title: '국문성분명',  key: 'krIngredientName', width: 200},
-    { title: '영문성분명',  key: 'enIngredientName', width: 200},
-    { title: '중문성분명',  key: 'cnIngredientName', width: 200},
-    { title: 'CAS No',     key: 'casNo', width:100},
-    { title: 'function',   key: 'functionNm', width:150},
-    { title: '수정자',      key: 'updNm', width:100},
-    { title: '수정일자',    key: 'updDate', width:100},
-    { title: '비고',        key: 'etc', width:100},
-])
-
-const handleRowClick = (item) => {
-  ingredientCode.value = item;
-  openPop('U')
-}
-
-const handleSaved = (msg) => {
-  vSuccess(msg)
-  srhList()
-}
-
-const mode = ref('')
-const title = ref('')
-
 const openPop = (gb) => {
   if (gb === 'N' ) {
      mode.value= 'N'
@@ -196,7 +197,13 @@ const openPop = (gb) => {
   dialog.value = true
 }
 
-onMounted(srhList)
+
+/**
+ * 초기화
+ */
+onMounted( async () =>{
+  srhList()
+})
 
 /**
  * 엑셀 다운로드
@@ -205,34 +212,9 @@ const excel = () => {
   exportToExcel(headers, ingredientList.value, '성분_목록')
 }
 
-/**
- *  검색창 초기화
- */
-const reset = () =>{
-  srhForm.value.reset()
-}
 
 </script>
 
-<style scoped>
+<style>
 @import '@/assets/css/main.css';
-
-/*
-.table-wrapper {
-  overflow-x: auto;
-}
-.scrollable-table ::v-deep .v-table {
-  min-width: 3000px;
-}
-
-.scrollable-table ::v-deep th.column-name,
-.scrollable-table ::v-deep td.column-name {
-  position: sticky;
-  left: 0;
-  background: white;
-  z-index: 2;
-  min-width: 150px;
-  max-width: 150px;
-}
-*/
 </style>
