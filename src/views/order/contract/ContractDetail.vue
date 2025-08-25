4dccac527fae8c7258274bc60f234e1f3f7bccfd
kimyt <kimyt@jincostech.com>
2025-08-25 12:45:07 +0900

    date component 변경

--- src/views/order/contract/ContractDetail.vue
+++ src/views/order/contract/ContractDetail.vue
@@ -26,18 +26,22 @@
         <v-col>
         </v-col>
         <v-col>
-          <DateSinglePicker
+          <v-date-input
             v-model="form.expiryDate"
             title="유효기간"
-            density="compact"
-            width="120"
+            :display-format="formatDate"
+            variant="underlined"
+            width="150"
             />
         </v-col>
         <v-col>
-          <DateSinglePicker
+          <v-date-input
             v-model="form.dueDate"
             title="납기일자"
+            :display-format="formatDate"
+            variant="underlined"
             density="compact"
+            width="150"
             />
         </v-col>
     </v-row>
@@ -232,7 +236,7 @@
           <template v-slot:body.append>
             <tr class="summary-row">
               <!-- itemCd -->
-              <td style="width: 100px; height: 30px;"></td>
+              <td style="width: 100px; height: 30px; text-align: center;">합계</td>
               <!-- itemName -->
               <td style="width: 250px; height: 30px;"></td>
               <!-- unit -->
@@ -487,6 +491,10 @@ const saveInfo = async () => {
       const params = {
         ...form
       }
+
+      params.contractDate = formatDate(params.contractDate)
+      params.dueDate = formatDate(params.dueDate)
+      params.expiryDate = formatDate(params.expiryDate)
 
       formData.append('contractInfo', JSON.stringify(params))
       formData.append('itemList', JSON.stringify(itemList.value))
