c5eb38d8201fd9f002de02c36ec1847bf9bfb382
kimyt <kimyt@jincostech.com>
2025-08-26 10:36:25 +0900

    다운로드 로직 수정

--- src/components/DownLoadLink.vue
+++ src/components/DownLoadLink.vue
@@ -1,43 +1,50 @@
 <!-- components/DownloadLink.vue -->
 <template>
-    <a :href="downloadUrl" :download="fileName" class="text-blue-600 underline" target="_blank" rel="noopener">
-      {{ fileName }}
+    <a
+      v-if="fileName"
+      href="#"
+      :download="fileName"
+      @click.prevent="downloadUrl"
+      class="text-blue-600 underline"
+      >{{ fileName }}
     </a>
 </template>
 
 <script setup>
-import { computed } from 'vue'
 import { download } from '@/util/filedownLoad';
+import { defineProps } from 'vue';
 
 const props = defineProps({
-  filepath: {
-    type: String,
-    default: '',
-    required: true
-  },
   fileName: {
     type: String,
     default: '',
     required: true
   },
   attachFileId: {
-    type: String,
+    type: [String, Number],
     default: '',
     required: true
   },
   seq: {
-    type: Number,
+    type: [String, Number],
     default: 1,
     required: true
   }
 })
 
-download(props.attachFileId, props.seq)
-// 다운로드 URL 생성
-// const downloadUrl = computed(() => {
-//   //return `/files/download?filepath=${encodeURIComponent(props.filepath)}`
+console.log('attachFileId', props.attachFileId)
+console.log('fileName', props.fileName)
+console.log('seq', props.seq)
 
-// })
+const downloadUrl = () =>{
+  console.log('props.attachFileId', props.attachFileId)
+  console.log('props.seq', props.seq)
+  download(props.attachFileId, props.seq)
+}
+// 다운로드 URL 생성
+//  const downloadUrl = computed(() => {
+//    return `/files/download?filepath=${encodeURIComponent(props.filepath)}`
+//  })
 </script>
 
 <style scoped>
