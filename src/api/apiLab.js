b75146aa2e3fd49e66a87dc5b774f8a320c9da99
kimyt <kimyt@jincostech.com>
2025-09-18 10:04:38 +0900

    원료 관리 추가

--- src/api/apiLab.js
+++ src/api/apiLab.js
@@ -1,7 +1,7 @@
 import { API_URL} from '.'
 
 export const ApiLab = {
-
+  // 성분관리
   getIngredientList: async(params) => {
     try{
       const res = await API_URL.post('/lab/getIngredientList', params)
@@ -32,6 +32,33 @@ export const ApiLab = {
     }
   },
 
+  //원료관리
+  getMaterialList: async(id) => {
+    try{
+      const res = await API_URL.get(`/lab/getMaterialList/${id}`)
+      return res.data
 
+    }catch(err){
+      throw err.response
+    }
+  },
+  getMaterialInfo: async(id) => {
+    try{
+      const res = await API_URL.get(`/lab/getMaterialInfo/${id}`)
+      return res.data
+
+    }catch(err){
+      throw err.response
+    }
+  },
+  saveMaterialInfo: async(formData) => {
+    try{
+      const msg =  await API_URL.post('/lab/saveMaterialInfo', formData)
+
+      return msg.data
+    }catch(err) {
+      throw new Error(err.response?.data);
+    }
+  },
 
 }
