a5af52fa1ae5bfb52e640c4b02f632ac49bb423a
kimyt <kimyt@jincostech.com>
2025-10-01 17:58:58 +0900

    신원료 신규 생성

---
+++ src/views/lab/newmaterial/NewMaterialDetail.vue
@@ -0,0 +1,270 @@
+<template>
+<div :id="$attrs.id"></div>
+<v-breadcrumbs :items="['MES', '연구관리', '신 원료상세']"></v-breadcrumbs>
+<v-card>
+  <v-card-text>
+    <v-form  @submit.prevent="saveInfo">
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="form.newMaterialCd"
+            label="임시코드"
+            variant="underlined"
+            density="compact"
+            readonly
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model="form.materialName"
+            label="원료명"
+            variant="underlined"
+            density="compact"
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model="form.inPrice"
+            label="입고단가"
+            variant="underlined"
+            density="compact"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="form.mtrName"
+            label="제조원"
+            variant="underlined"
+            density="compact"
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model="form.customerName"
+            label="거래처"
+            variant="underlined"
+            density="compact"
+            append-inner-icon="mdi-magnify"
+            @click:append-inner="openPop('C')"
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model="form.itemCd"
+            label="품목코드"
+            variant="underlined"
+            density="compact"
+            append-inner-icon="mdi-magnify"
+            @click:append-inner="openPop('P')"
+          />
+        </v-col>
+      </v-row>
+      <v-row class="mt-4">
+        <v-col class="mt-3">
+          <v-card-subtitle>
+            - 성분코드
+          </v-card-subtitle>
+        </v-col>
+        <v-col class="d-flex justify-end">
+          <v-btn
+            color="brown-lighten-4"
+            class="mt-3"
+            text="추가+"
+            density="compact"
+            @click="openPop('I')"
+            />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-data-table-virtual
+            :headers="headers"
+            :items="materialMappingList"
+            class="custom-table"
+          >
+          <template #item.inContent="{item, index}">
+              <input
+                v-model="materialMappingList[index].inContent"
+                style="text-align: right; width: 90%; height: 30px;"
+                density="compact"
+                class="custom-line"
+                />
+          </template>
+          <template #item.outContent="{ item, index }">
+            <input
+              v-model="materialMappingList[index].outContent"
+              style="text-align: right; width: 90%; height: 30px;"
+              class="custom-line"
+            />
+          </template>
+          <template #item.actions="{ item }">
+            <v-icon color="medium-emphasis" icon="mdi-delete" size="small" @click="removeRow(item.ingredientCode)"></v-icon>
+          </template>
+
+        </v-data-table-virtual>
+      </v-row>
+      <v-row>
+        <v-col class="d-flex justify-end mt-3 ga-3">
+          <v-btn
+            text="저장"
+            color="brown-lighten-4"
+            type="submit"
+            ></v-btn>
+          <v-btn
+            text="목록"
+            @click="goList"
+          ></v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-card-text>
+</v-card>
+
+<v-dialog v-model="dialog" width="800px" height="800px" persistent>
+    <component
+      :is="currentComponent"
+      :id="id"
+      @selected="handleselect"
+      @close-dialog="dialog = false"
+    />
+</v-dialog>
+
+</template>
+
+<script setup>
+import { ApiLab } from '@/api/apiLab';
+import { isEmpty } from '@/util/common';
+import { onMounted, reactive, ref, shallowRef } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import IngredientListPop from '../ingredient/IngredientListPop.vue';
+import CustomerListPop from '@/views/basic/customer/CustomerListPop.vue';
+import ItemListSinglePop from '@/views/basic/item/ItemListSinglePop.vue';
+import { useAlertStore } from '@/stores/alert';
+import { useAuthStore } from '@/stores/auth';
+
+const { userId } = useAuthStore()
+const {vError, vSuccess, vInfo} = useAlertStore()
+const route = useRoute()
+const router = useRouter()
+const newMaterialCd = route.params.id
+const dialog = ref(false)
+const selectPop = ref('')
+const currentComponent = shallowRef(null)
+const materialMappingList = ref([])
+const form = reactive({
+  newMaterialCd: '',
+  itemCd : '',
+  itemName : '',
+  materialName: '',
+  inPrice: '',
+  customerCd: '',
+  customerName: '',
+  mtrName: '',
+
+  userId: userId,
+})
+
+const headers = [
+    { title: '성분코드',     key: 'ingredientCode',    align: 'center',  width:'90px'},
+    { title: '국문성분명',   key: 'krIngredientName',  align: 'start', width: '230px'},
+    { title: '영어성분명',   key: 'enIngredientName',  align: 'start', width: '230px'},
+    { title: '국내함량',     key: 'inContent',         align: 'center', width: '100px'},
+    { title: '수출용함량',   key: 'outContent',        align: 'center', width: '100px'},
+    { title: 'CAS No',      key: 'casNo',             align: 'center',width: '90px'} ,
+    { title: 'function',    key: 'functionNm',        align: 'center',width: '90px'},
+    { title: '한도 국가',    key: 'limitCountry',      align: 'center',width: '90px'},
+    { title: '금지 국가',    key: 'banCountry',        align: 'center',width: '90px'},
+    { title: '-',           key: 'actions',           align: 'center' ,width : '10px'},
+]
+
+const saveInfo = async () =>{
+  try{
+    const params = {
+      newMaterialInfo : form,
+      materialMappingList : materialMappingList.value,
+    }
+
+    const res = await ApiLab.saveNewMaterialInfo(params)
+    form.newMaterialCd  = res.newMaterialCd
+    vSuccess("저장되었습니다.")
+  }catch(err){
+    vError("저장에 실패했습니다.")
+  }
+}
+
+const openPop = (type) =>{
+  selectPop.value = type
+
+  if ( type === 'I'  ) {
+    currentComponent.value = IngredientListPop
+  }else if ( type === 'C'  ) {
+    currentComponent.value = CustomerListPop
+  }else if ( type === 'P'  ) {
+    currentComponent.value = ItemListSinglePop
+  }
+
+  dialog.value = true
+}
+
+const handleselect = (obj) => {
+  if ( selectPop.value === 'C'  ) {
+    form.customerCd = obj.customerCd
+    form.customerName = obj.customerName
+  }else if ( selectPop.value === 'P'  ) {
+    form.itemCd = obj.itemCd
+    form.itemName = obj.itemName
+   }else if ( selectPop.value === 'I'  ) {
+    ingredientList(obj)
+   }
+
+}
+
+const ingredientList = (obj) =>{
+  if (!Array.isArray(obj)) return;
+  let nextId = materialMappingList.value.length;
+
+  const selectItem = obj.map((o, index) => ({
+      id: nextId++,
+      ingredientCode: o.ingredientCode,
+      krIngredientName: o.krIngredientName,
+      enIngredientName: o.enIngredientName,
+      inContent: '',
+      outContent: '',
+      casNo: o.casNo,
+      functionNm: o.functionNm,
+      limitCountry: o.limitCountry,
+      banCountry: o.banCountry,
+  }));
+
+  if (materialMappingList.value.length > 0) {
+    materialMappingList.value.push(...selectItem);
+  } else {
+    materialMappingList.value = [...selectItem];
+  }
+}
+
+const removeRow =(id) => {
+  const index = materialMappingList.value.findIndex(ingredient => ingredient.ingredientCode === id)
+  materialMappingList.value.splice(index, 1)
+}
+
+onMounted( async () =>{
+  if (!isEmpty( newMaterialCd )) {
+    const res = await ApiLab.getNewMaterialInfo(newMaterialCd)
+
+    Object.assign(form, res.newMaterialInfo)
+
+    materialMappingList.value = res.materialMappingList
+  }
+})
+
+const goList = () =>{
+  router.push({name:'NewMaterialList'})
+}
+
+</script>
+
+<style scoped>
+@import '@/assets/css/main.css';
+</style>
---
+++ src/views/lab/newmaterial/NewMaterialList.vue
@@ -0,0 +1,106 @@
+<template>
+<v-breadcrumbs :items="['MES', '연구관리', '신원료리스트']" class="custom-breadcrumbs"/>
+  <v-card class="pa-1" style="height: 60px;">
+    <v-card-text >
+      <v-row>
+        <v-form ref="srhForm" @submit.prevent="srhNewMaterialList">
+          <v-col class="d-flex ga-4">
+            <v-text-field
+              v-model="form.materialName"
+              label="품목명"
+              density="compact"
+              variant="underlined"
+              placeholder="품목명을 입력해주세요"
+              style="width: 200px;"
+            />
+            <v-text-field
+              v-model="form.regName"
+              label="등록자."
+              density="compact"
+              variant="underlined"
+              placeholder="등록자를 입력해주세요"
+              style="width: 150px;"
+            />
+            <v-btn
+              text="조회"
+              color="brown-lighten-4"
+              type="submit"
+              />
+          </v-col>
+        </v-form>
+      </v-row>
+    </v-card-text>
+  </v-card>
+  <v-spacer></v-spacer>
+  <v-row>
+    <v-col class="d-flex justify-end align-center mr-2" style="gap: 10px; margin-top: 10px;">
+      <v-btn
+        color="brown-lighten-4"
+        text="신규"
+        @click="selectRowClick"
+        />
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col class="pa-0">
+      <v-data-table
+        :headers="headers"
+        :items="newMaterialList"
+        density="compact"
+        fixed-header
+        :items-per-page="15"
+        height="720px"
+        class="custom-table"
+        >
+        <template #item.materialName="{ item, index }">
+          <div
+            style="cursor: pointer; text-decoration: underline;"
+            @click="selectRowClick(item, index)"
+          >
+            {{ item.materialName }}
+          </div>
+        </template>
+      </v-data-table>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+import { ApiLab } from '@/api/apiLab';
+import { reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const srhForm = ref('')
+const newMaterialList = ref([])
+const form = reactive({
+  materialName: '',
+  regName: '',
+})
+
+const headers = [
+  { title: 'No.',       key: 'rowNum',        align: 'center', width: '30px' },
+  { title: '임시코드',   key: 'newMaterialCd', align: 'center',  width: '150px' },
+  { title: '품목명',    key: 'materialName',   align: 'start',  width: '480px' },
+  { title: '등록자',    key: 'regName',        align: 'center',    width: '120px' },
+  { title: '등록일자',  key: 'regDate',        align: 'center',    width: '110px' },
+  { title: '품목코드',   key: 'itemCd',        align: 'center',  width: '150px' },
+]
+
+const srhNewMaterialList = async ()=>{
+  const params = {
+    ...form
+  }
+
+  newMaterialList.value = await ApiLab.getNewMaterialList(params)
+}
+
+const selectRowClick = (item, index) =>{
+  router.push({name: 'NewMaterialDetail',params: { id: item.newMaterialCd }  })
+}
+
+</script>
+
+<style scoped>
+@import '@/assets/css/main.css';
+</style>
