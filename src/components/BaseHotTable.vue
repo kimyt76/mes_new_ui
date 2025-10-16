73f8f18e5a2522964141bd8916f371e611169793
kimyt <kimyt@jincostech.com>
2025-10-16 11:39:28 +0900

    엑셀용 테이블 공통 추가

---
+++ src/components/BaseHotTable.vue
@@ -0,0 +1,32 @@
+<template>
+  <HotTable
+    ref="innerHot"
+    :licenseKey="'non-commercial-and-evaluation'"
+    v-bind="$attrs"
+    :theme="'htThemeMain'"
+  />
+</template>
+
+<script setup>
+import { HotTable} from '@handsontable/vue3'
+import { ref } from 'vue'
+
+const innerHot = ref(null)
+defineExpose({
+  hotInstance: innerHot,
+})
+
+
+</script>
+
+<style>
+/* modern 테마 사용 */
+@import 'handsontable/styles/handsontable.min.css';
+@import 'handsontable/styles/ht-theme-main.min.css';
+/* header 색상 지정*/
+.ht_clone_top th {
+  background-color: #FFE0B2;
+  color: #000;
+}
+
+</style>
