a59706917a45096376bb1232c5f219b8e0c1230d
kimyt <kimyt@jincostech.com>
2026-03-04 11:18:06 +0900

    발주현황 신규

---
+++ src/views/purchase/order/PurchaseOrderDetail1Pop.vue
@@ -0,0 +1,194 @@
+<template>
+<Card style="width: 90rem; height: 10rem;">
+    <template #content>
+        <!-- Row 1 -->
+        <div class="grid mb-1">
+            <div class="col-3 flex align-items-center gap-2">
+                <FloatLabel variant="on">
+                    <DatePicker v-model="form.purOrderDate" readonly/>
+                    <label>발주일</label>
+                </FloatLabel>
+                <span class="center-dash">-</span>
+                <FloatLabel variant="on">
+                    <InputNumber v-model="form.seq" :inputStyle="{ width: '50px', 'text-align': 'center' }"  readonly/> <!-- 크기 축소 -->
+                    <label>연번</label>
+                </FloatLabel>
+            </div>
+            <div class="col-3">
+                <FloatLabel variant="on">
+                    <Select v-model="form.itemTypeCd"
+                            :options="itemTypeCds"
+                            optionLabel="codeNm"
+                            optionValue="code" class="w-full" readonly/>
+                    <label>품목구분</label>
+                </FloatLabel>
+            </div>
+            <div class="col-3">
+                <FloatLabel variant="on">
+                    <IconField iconPosition="left">
+                        <InputText v-model="form.storageName" class="w-full"/>
+                    </IconField>
+                    <label>입고창고</label>
+                </FloatLabel>
+            </div>
+            <div class="col-3">
+                <FloatLabel variant="on">
+                    <IconField iconPosition="left">
+                        <InputText v-model="form.managerName" class="w-full"/>
+                    </IconField>
+                    <label>담당자</label>
+                </FloatLabel>
+            </div>
+        </div>
+        <!-- Row 1 -->
+        <div class="grid mb-1">
+            <div class="col-3">
+                <FloatLabel variant="on">
+                    <DatePicker v-model="form.deliveryDate"  show-icon fluid/>
+                    <label>납기일</label>
+                </FloatLabel>
+            </div>
+            <div class="col-3">
+                <FloatLabel variant="on">
+                    <IconField iconPosition="left">
+                        <InputText v-model="form.customerName" class="w-full"/>
+                    </IconField>
+                    <label>거래처</label>
+                </FloatLabel>
+            </div>
+            <div class="col-3">
+                <FloatLabel variant="on">
+                    <InputText v-model="form.customerManagerName" class="w-full"/>
+                    <label>거래처담당자</label>
+                </FloatLabel>
+            </div>
+            <div class="col-3">
+                <FloatLabel variant="on">
+                    <Select v-model="form.vatType"
+                            :options="vatTypes"
+                            optionLabel="codeNm"
+                            optionValue="code" class="w-full" readonly/>
+                    <label>부가세율적용</label>
+                </FloatLabel>
+            </div>
+        </div>
+         <!-- Row 1 -->
+        <div class="grid mb-1">
+            <div class="col-12">
+                <FloatLabel variant="on">
+                    <Textarea v-model="form.remark" class="w-full" :rows="1" style="resize: none;" readonly/>
+                    <label>비고</label>
+                </FloatLabel>
+            </div>
+        </div>
+    </template>
+</Card>
+
+<div class="w-full mt-2">
+    <DataTable
+        :value="purchaseOrderItemList"
+        selectionMode="single"
+        class="my-table"
+        show-gridlines
+        >
+        <Column field="itemCd"    header="품목코드"  :style="{ width: '90px'}" />
+        <Column field="itemName"  header="품목명"    :style="{ width: '350px'}" bodyClass="break-words"/>
+        <Column field="spec"      header="규격"      :style="{ width: '110px'}"/>
+        <Column field="qty"        header="수량"    :style="{ width: '80px'}"  >
+            <template #body="slotProps">{{ Number(slotProps.data.totQty).toLocaleString() }}</template>
+        </Column>
+        <Column field="inPrice"        header="단가"    :style="{ width: '80px'}" >
+            <template #body="slotProps">{{ Number(slotProps.data.inPrice).toLocaleString() }}</template>
+        </Column>
+        <Column field="supplyPrice"        header="공급가액"   :style="{ width: '80px'}" >
+        <template #body="slotProps">{{ Number(slotProps.data.supplyPrice).toLocaleString() }}</template>
+        </Column>
+        <Column field="vatPrice"        header="부가세"    :style="{ width: '80px'}" >
+            <template #body="slotProps">{{ Number(slotProps.data.vatPrice).toLocaleString() }}</template>
+        </Column>
+        <Column field="etc"        header="비고"    :style="{ width: '200px'}" style="text-align: right;" />
+    </DataTable>
+</div>
+<div class="w-full flex gap-2 justify-end mt-2">
+    <Button label="닫기" outlined class="ml-2" @click="closeDialog" />
+</div>
+</template>
+
+<script setup>
+import { ApiCommon } from '@/api/apiCommon';
+import { ApiPurchase } from '@/api/apiPurchase';
+import { InputNumber, InputText } from 'primevue';
+import { inject, onMounted, reactive, ref } from 'vue';
+
+const dialogRef = inject('dialogRef');
+const itemTypeCds = ref([])
+const vatTypes = ref([])
+const purchaseOrderItemList = ref([])
+const form = reactive({
+    purOrderDate:'',
+    seq: 0,
+    itemTypeCd: '',
+    storageCd: '',
+    storageName: '',
+    managerName: '',
+    managerId: '',
+    deliveryDate: '',
+    customerName: '',
+    customerCd: '',
+    customerManagerName: '',
+    remark: '',
+    vatType: '',
+
+    mailYn : '',
+    printYn : '',
+    inYn: '',
+    endYn: '',
+    areaCd: '',
+    purOrderId: '',
+})
+
+onMounted( async () => {
+    itemTypeCds.value =await ApiCommon.getCodeList('item_type_cd')
+    vatTypes.value = await ApiCommon.getCodeList('vat_type')
+
+    const params = {
+        itemTypeCd: dialogRef.value.data.itemTypeCd,
+        purOrderId: dialogRef.value.data.id
+    }
+
+    const res = await ApiPurchase.getPurchaseOrderInfo(params)
+
+    Object.assign(form, res.purchaseOrderInfo)
+    purchaseOrderItemList.value =res.purchaseOrderItemList
+})
+
+const closeDialog = () =>{
+    dialogRef.value.close()
+}
+
+</script>
+
+<style scoped>
+/* 셀 hover 효과 */
+.clickable-cell {
+  cursor: pointer;
+  padding: 0.25rem 0;
+  text-decoration: underline;
+  text-align: left;
+}
+::v-deep(.my-table .p-datatable-thead > tr > th) {
+  background-color: #BCAAA4;
+  color: white;
+  font-size: 14px;
+  text-align: center;
+  font-family: monaco, Consolas;
+}
+
+.center-dash {
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+  height: 100%;
+}
+
+</style>
---
+++ src/views/purchase/order/PurchaseOrderPDetailList.vue
@@ -0,0 +1,242 @@
+<template>
+<Breadcrumb :home="home" :model="items"/>
+<form @submit.prevent="srhList" class="space-y-4">
+    <Toolbar class="flex flex-wrap mt-2 mb-2 gap-1 w-full"  >
+        <template #start>
+            <div class="flex flex-wrap items-center gap-2 w-full">
+            <FloatLabel variant="on">
+                <DatePicker v-model="form.strDate" inputId="on_label" showIcon iconDisplay="input" />
+                <label for="on_label">시작</label>
+            </FloatLabel>
+            <FloatLabel variant="on">
+                <DatePicker v-model="form.toDate" inputId="on_label" showIcon iconDisplay="input" />
+                <label for="on_label">종료</label>
+            </FloatLabel>
+            <FloatLabel variant="on">
+                <Select v-model="form.areaCd" :options="areaCds"
+                   optionLabel="codeNm"
+                   optionValue="code"
+                style="width: 120px"
+                />
+                <label for="on_label">구역(공장)</label>
+            </FloatLabel>
+            <FloatLabel variant="on">
+                <Select v-model="form.itemTypeCd" :options="itemTypeCds"
+                   optionLabel="codeNm"
+                   optionValue="code"
+                style="width: 120px"
+                />
+                <label for="on_label">품목구분</label>
+            </FloatLabel>
+            <FloatLabel variant="on">
+                <InputText id="on_label" v-model="form.itemName" style="width: 180px"/>
+                <label for="on_label">품목명</label>
+            </FloatLabel>
+            <FloatLabel variant="on">
+                <InputText id="on_label1" v-model="form.itemCd" style="width: 120px" />
+                <label for="on_label1">품목코드</label>
+            </FloatLabel>
+            <FloatLabel variant="on">
+                <InputText id="on_label1" v-model="form.customerName" style="width: 180px" />
+                <label for="on_label1">거래처명</label>
+            </FloatLabel>
+            <FloatLabel variant="on">
+                <Select v-model="form.regYn" :options="regYns"
+                   optionLabel="codeNm"
+                   optionValue="code"
+                style="width: 90px"
+                />
+                <label for="on_label">입고상태</label>
+            </FloatLabel>
+            <FloatLabel variant="on">
+                <Select v-model="form.endYn" :options="endYns"
+                   optionLabel="codeNm"
+                   optionValue="code"
+                style="width: 90px"
+                />
+                <label for="on_label">진행상태</label>
+            </FloatLabel>
+            <Button label="검색" icon="pi pi-search" type="submit" class="bg-blue-500 text-white hover:bg-blue-600" />
+            </div>
+        </template>
+    </Toolbar>
+</form>
+<div class="flex items-center justify-end gap-2 mb-2">
+    <Button label="신규" icon="pi pi-plus" severity="secondary" @click="selectRowClick('')"></Button>
+    <Button label="엑셀" icon="pi pi-file-excel" severity="success" @click="downloadExcel"></Button>
+    <Button label="인쇄" icon="pi pi-print"  outlined @click="print"></Button>
+</div>
+<div>
+    <DataTable
+        ref="dt"
+        v-model:selection="selectedItem"
+        :value="purchaseOrderList"
+        dataKey="purOrderId"
+        paginator :rows="20"
+        :rowsPerPageOptions="[20,30,40]"
+        scrollHeight="700px"
+        scrollable
+        showGridlines
+        selectionMode="single"
+        tableStyle="min-width:120rem; table-layout: fixed;"
+        class="my-table"
+        >
+        <Column selectionMode="multiple"    headerStyle="width: 3rem" style="text-align: center;"></Column>
+        <Column field="purOrderDateSeq"     header="일자"  frozen :style="{ width: '140px', textAlign:'center'}" />
+        <Column field="purOrderDate"  header="발주일"  frozen :style="{ width: '110px', textAlign:'center'}" />
+        <Column field="deliveryDate"  header="납기일"  :style="{ width: '110px', textAlign:'center'}" />
+        <Column field="customerName"  header="거래처명"  :style="{ width: '250px'}" />
+        <Column field="itemName"      header="품목명"    :style="{ width: '400px'}" bodyClass="break-words" >
+            <template #body="slotProps">
+                <div @click="selectRowClick(slotProps.data.purOrderId)" class="clickable-cell">
+                    {{ slotProps.data.itemName }}
+                </div>
+            </template>
+        </Column>
+        <Column field="storageName"   header="입고창고"  :style="{ width: '130px', textAlign:'center'}" />
+        <Column field="totQty"        header="발주수량"   :style="{ width: '90px', textAlign:'right'}" >
+            <template #body="slotProps">{{ Number(slotProps.data.totQty).toLocaleString() }}</template>
+        </Column>
+        <Column field="supplyPrice" header="공급가액"    :style="{ width: '90px', textAlign:'right'}">
+            <template #body="slotProps">{{ Number(slotProps.data.supplyPrice).toLocaleString() }}</template>
+        </Column>
+        <Column field="inYn"        header="입고상태"   :style="{ width: '90px', textAlign:'center'}" />
+        <Column field="endYn"       header="진행상태"   :style="{ width: '90px', textAlign:'center'}" />
+        <Column field="mailYn"      header="발주서발송" :style="{ width: '100px', textAlign:'center'}" />
+        <Column field="managerName" header="담당자"     :style="{ width: '90px', textAlign:'center'}" />
+    </DataTable>
+</div>
+
+</template>
+
+<script setup>
+import { ApiCommon } from '@/api/apiCommon';
+import { ApiPurchase } from '@/api/apiPurchase';
+import { minMonth, todayKST } from '@/util/common';
+import { exportToExcel } from '@/util/exportToExcel';
+import { useDialog } from 'primevue';
+import { onMounted, reactive, ref } from 'vue';
+import PurchaseOrderDetail1Pop from './PurchaseOrderDetail1Pop.vue';
+
+const matOrderDialog = ref(false)
+// 리스트에서 체크박스,  radio 있을 경우
+const selectedItem = ref([]);
+const dialog = useDialog()
+const dt = ref(null);
+const areaCds = ref([])
+const itemTypeCds = ref([])
+const regYns = ref([
+    { codeNm: '미입고', code: 'N' },
+    { codeNm: '입고', code: 'Y' },
+])
+const endYns = ref([
+    { codeNm: '진행중', code: 'N' },
+    { codeNm: '종결', code: 'Y' },
+])
+
+const form = reactive({
+    strDate: '',
+    toDate: '',
+    itemTypeCd: 'M2',
+    itemCd: '',
+    itemName: '',
+    customerName: '',
+    customerCd: '',
+    regYn: '',
+    endYn: '',
+    selectedCity: '',
+})
+
+//리스트
+const purchaseOrderList = ref([])
+
+const selectRowClick = (id) =>{
+    dialog.open(PurchaseOrderDetail1Pop, {
+        props:{
+            header: '발주서 상세',
+            modal: true,
+            maximizable: false,
+            draggable: true,
+            style: {
+                overflow: 'hidden'
+            },
+            pt: {
+                root: { style: { overflow: 'hidden' } },
+                content: { style: { overflow: 'hidden' } }
+            },
+        },
+        data: {
+            id : id,
+            itemTypeCd : form.itemTypeCd
+        },
+        onClose:(event) => {
+            srhList()
+        },
+    })
+}
+
+const srhList = async () =>{
+    const params = {
+        ...form
+    }
+    // api
+    purchaseOrderList.value = await ApiPurchase.getPurchaseOrderList(params);
+}
+
+const print = () => {
+
+}
+
+onMounted( async () => {
+    areaCds.value = await ApiCommon.getCodeList('AREA');
+    itemTypeCds.value = await ApiCommon.getCodeList('ITEM_TYPE_CD');
+
+    form.toDate = todayKST()
+    form.strDate = minMonth(form.toDate)
+})
+
+const home = ref({
+    icon: 'pi pi-home'
+});
+const items = ref([
+    { label: '발주관리' },
+    { label: '발주현황(구매)' },
+    { label: '발주현황(구매)목록' },
+]);
+
+const downloadExcel = () =>{
+  const cols = dt.value?.columns ?? [];
+
+  if (!cols.length) {
+    console.warn("No Columns Found");
+    return;
+  }
+  exportToExcel(matOrderlist.value, "발주 리스트", cols);
+}
+
+
+</script>
+
+<style scoped>
+::v-deep(.my-table .p-datatable-thead > tr > th) {
+  background-color: #BCAAA4;
+  color: white;
+  font-size: 14px;
+  text-align: center;
+  font-family: monaco, Consolas;
+}
+::v-deep(.break-words) {
+  white-space: normal;
+  word-break: break-word;
+  overflow-wrap: anywhere;
+  text-decoration: underline;
+  cursor: pointer;
+}
+.clickable-cell {
+  cursor: pointer;
+  padding: 0.25rem 0;
+  text-decoration: underline;
+  text-align: left;
+}
+
+</style>
