f6fdc4f365f14f4cd0fbc9b8f6fe3b142196b6e1
kimyt <kimyt@jincostech.com>
2026-03-25 17:24:28 +0900

    공정검사 목록 신규

--- src/views/qc/procTest/ProcTestList.vue
+++ src/views/qc/procTest/ProcTestList.vue
@@ -1,13 +1,41 @@
 <template>
-    <div>
-
-    </div>
+<Breadcrumb :home="home" :model="items"/>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+
+const home = ref({
+    icon: 'pi pi-home'
+});
+const items = ref([
+    { label: '공정관리' },
+    { label: '공정검사' },
+    { label: '공정검사목록' },
+]);
 
 </script>
 
 <style  scoped>
-
+::v-deep(.my-table .p-datatable-thead > tr > th) {
+  background-color: #BCAAA4;
+  color: white;
+  font-size: 14px;
+  text-align: center;
+  font-family: monaco, Consolas;
+}
+::v-deep(.break-words) {
+  white-space: normal;
+  word-break: break-word;
+  overflow-wrap: anywhere;
+  text-decoration: underline;
+  cursor: pointer;
+}
+.clickable-cell {
+  cursor: pointer;
+  padding: 0.25rem 0;
+  text-decoration: underline;
+  text-align: left;
+}
 </style>
