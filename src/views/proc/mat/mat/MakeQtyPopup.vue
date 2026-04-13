a03bfac0bec533bad092fc0e93c81c8d31caf375
kimyt <kimyt@jincostech.com>
2026-04-13 11:01:48 +0900

    제조량 수동입력창 신규

---
+++ src/views/proc/mat/mat/MakeQtyPopup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="mt-3">
+    <div>
+        <FloatLabel variant="on">
+            <InputNumber
+                v-model="makeQty"
+                :min="0"
+                :maxFractionDigits="6"
+                :useGrouping="false"
+            />
+            <label for="on_label1">투입량 입력</label>
+        </FloatLabel>
+    </div>
+    <div class="flex justify-end mt-2">
+        <Button
+          label="저장"
+          @click="save"
+          ></Button>
+    </div>
+</div>
+</template>
+
+<script setup>
+import { inject, onMounted, ref } from 'vue';
+
+const dialogRef = inject('dialogRef')
+const makeQty = ref(0)
+
+const save = () =>{
+    dialogRef.value.close(makeQty.value)
+}
+
+onMounted(() =>{
+    console.log('dialogRef.value', dialogRef.value )
+    console.log('dialogRef.data', dialogRef.value.data )
+    makeQty.value = dialogRef.value.data.row.orderQty
+})
+
+</script>
+
+<style  scoped>
+
+</style>
